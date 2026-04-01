5ab364a2b08f1571995afac59784da1121f5d65d
Parag-05 <guptaparag138@gmail.com>
2026-04-01 22:37:31 +0530

    Display Hello with Command-Line Argument

--- HelloApp.java
+++ HelloApp.java
@@ -1,8 +1,10 @@
 public class HelloApp {
-   public HelloApp() {
-   }
+    public static void main(String[] args) {
 
-   public static void main(String[] var0) {
-      System.out.println("Hello World");
-   }
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello User");
+        }
+    }
 }

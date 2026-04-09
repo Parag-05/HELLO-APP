3b2d9b07c76646fa7902716b636c4aa42b6864ed
Parag-05 <guptaparag138@gmail.com>
2026-04-09 23:30:44 +0530

    Display Multiple Names

--- HelloApp.java
+++ HelloApp.java
@@ -1,14 +1,17 @@
 public class HelloApp {
     public static void main(String[] args) {
-
-        String name;
-
-        if (args.length > 0) {
-            name = args[0];
-        } else {
-            name = "World";
+        StringBuilder nameBuilder = new StringBuilder();
+        for(int i = 0; i < args.length; i++) {
+            nameBuilder.append(args[i]);
+            if(i < args.length - 1) {
+                nameBuilder.append(", ");
+            }
         }
-
-        System.out.println("Hello " + name);
+        String name = nameBuilder.toString();
+        if(args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            System.out.println("Hello, " + name + "!");
+        }
     }
 }
